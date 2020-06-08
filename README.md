a0c985e59e8d3b6bbf2fb0f1f3a82a99c8610eb4
Vladyslav Pekker <agilesteel@gmail.com>
2020-06-07 21:40:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installs [docker-compose](https://docs.docker.com/compose/) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/docker-compose
+sudo chmod +x docker-compose && ./docker-compose
+```
+
+# Specifies [version](https://github.com/docker/compose/releases) (optional)
+
+```bash
+wget -q https://script.install.devinsideyou.com/docker-compose
+sudo chmod +x docker-compose && ./docker-compose 1.26.0
+```
---
+++ install-docker-compose.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+FALLBACK_VERSION="1.26.0"
+
+if [ "$1" == "" ]; then
+    VERSION="$FALLBACK_VERSION"
+else
+    VERSION="$1"
+fi
+
+# install curl
+sudo apt install -yqqq curl
+
+SOURCE="https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-Linux-x86_64"
+TARGET="/usr/local/bin/docker-compose"
+
+# install docker-compose
+sudo curl -L $SOURCE -o $TARGET
+sudo chmod +x $TARGET
+
+echo
+echo '"docker-compose" is now on the path'
